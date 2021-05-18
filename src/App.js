1b2a845a40b310558b08746414fd8e2a214b2ca1
KCMicheal <kcmicheal@users.noreply.github.com>
2021-05-18 12:56:12 +0100

    redesigned landing page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Container, Card, CardContent, makeStyles, Grid, TextField, Button } from '@material-ui/core';
-import './App.css';
 import GenerateQrCode from './components/GenerateQrCode'
 import UploadToScan from './components/UploadToScan';
 import ScanByCamera from './components/ScanByCamera';
 import Home from "./Home"
 
+
 function App() {
 
-  const classes = useStyles();
 
   return (
-    <Container className={classes.container}>
       <Router>
         <Switch>
-          <Route exact path="/">
+          <Route exact path="/">/
             <Home/>
           </Route>
           <Route path="/generateqr">
@@ -29,28 +26,9 @@ function App() {
           </Route>
         </Switch>
       </Router>
-       
-    </Container>
   );
 }
 
-const useStyles = makeStyles((theme) => ({
-  container: {
-    marginTop: 10
-  },
-  title: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: 'center',
-    background: "tomato",
-    color: "#000",
-    padding: 20
-  },
-  btn: {
-    marginTop: 10,
-    marginBottom : 20,
-    backgroundColor: 'tomato'
-  }
-}));
+
 
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,76 @@
-import React, { useState } from 'react';
-import { Container, Card, CardContent, makeStyles, Grid, TextField, Button } from '@material-ui/core';
+import { Container, makeStyles, Grid, Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
 
 const Home = () => {
+
+    const classes =  useStyles();
     return (
-        <Container>
-            <Link to="/generateqr"><button>Oga</button></Link>
+        <Container className={classes.window}>
+            <div className={classes.nav}>
+               <h3>QR CODE APP</h3>
+           </div>
+
+           <Grid className={classes.jumbo}>
+               <h1>Generate, Upload & Scan</h1>
+           </Grid>
+           
+           <Grid className={classes.grid}>
+               <Link to="/generateqr" style={{textDecoration: "none"}}><Button className={classes.btn} variant="outlined" size="large">Generate QR Code</Button></Link>
+               <Link to="/upload" style={{textDecoration: "none"}}><Button className={classes.btn} variant="outlined" size="large">Upload & Scan</Button></Link>
+               <Link to="/scan" style={{textDecoration: "none"}}><Button className={classes.btn} variant="outlined" size="large">Scan QR Code</Button></Link>
+           </Grid>
+           
         </Container>
+           
+    
     )
 }
+
+const useStyles = makeStyles((theme) => ({
+    window: {
+        height: "100vh"
+    },
+    nav: {
+        display: "flex",
+        padding: "10px 10px",
+        marginBottom: "20px",
+        marginTop: "5px",
+        background: "transparent",
+        // boxShadow: "0px 5px 30px black",
+        color: "#fff",
+        alignItems: "center",
+        textAlign: "center",
+        justifyContent: "flex-start",
+        borderRadius: "2px",
+        width: "95%",
+        fontWeight: "bolder",
+    },
+    jumbo: {
+        display: "flex",
+        padding: "0px 50% 0px 0px",
+        color: "#fff",
+        minHeight: "50vh",
+        textAlign: "left",
+        justifyContent: "left",
+        alignItems: "center",
+        fontWeight: "900px",
+        fontSize: "40px",
+        textShadow: "300px",
+
+    },
+    grid: {
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+        textAlign: "center",
+        padding: "0"
+    },
+    btn: {
+        display: "flex-item",
+        color: "#fff",
+        borderColor: "#fff"
+    }
+}))
 
 export default Home
